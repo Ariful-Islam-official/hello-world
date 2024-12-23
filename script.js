2cbe5c9ebcc577bb32b8f9cc612ddc6db817c872
Ariful Islam <65085237+Ariful-Islam-official@users.noreply.github.com>
2024-12-23 01:43:08 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let header2 = document.querySelector("h2")     //<h1>DOM class</h1>
+//using append propatry
+// header2.append(" EDGE program")
+
+//innertext
+header2.innerText = header2.innerText + " EDGE program" //concatenation
+
